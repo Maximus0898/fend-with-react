42b9aaaa2adff303ea2ec1afc16451d50c009c26
Sarvar <r0ssmike727@yandex.com>
2020-08-30 01:37:04 +0500

    SPA Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
-
+import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 
@@ -20,48 +20,55 @@ function RenderDish({dish}) {
     } else {
       return <div></div>;
     }
-  }
+  };
 
 function  RenderComments({comments}) {
-  		if (comments == null) {
-            return (<div></div>)
-        }
-        const coms = comments.map(comment => {
-            return (
-                <li key={comment.id}>
-                    <p>{comment.comment}</p>
-                    <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', {year:'numeric', month:'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}
-                    </p>
-                </li>
-            )
-        })
-        return (
-            <div className='col-12 col-md-5 m-1'>
+  		if (comments != null) {
+          return (
+              <div className='col-12 col-md-5 m-1'>
                 <h4> Comments </h4>
                 <ul className='list-unstyled'>
-                    {coms}
+                {comments.map((comment) => {
+                   return (
+                       <li key={comment.id}>
+                           <p>{comment.comment}</p>
+                           <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', {year:'numeric', month:'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}
+                           </p>
+                       </li>
+                   );
+               })}
                 </ul>
-
             </div>
-        )
-  	};
+        );
+  	} else {
+      return (<div></div>);
+    }
+  }
+
 
 const DishDetail = (props) => {
-	const dish = props.dish;
-        if (dish == null) {
+        if (props.dish == null) {
             return (<div></div>)
         }
-      const dishItem = <RenderDish dish={dish}/>;
-      const commentItem = <RenderComments comments={dish.comments}/>;
 
       return (
           <div className="container">
-            <div className='row'>
-                {dishItem}
-                {commentItem}
+          <div className="row">
+            <Breadcrumb>
+              <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+              <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+              <h3>{props.dish.name}</h3>
+              <hr/>
+            </div>
+          </div>
+            <div className="row">
+            <RenderDish dish={props.dish}/>;
+            <RenderComments comments={props.comments}/>;
             </div>
           </div>
         );
-	};
+};
 
 export default DishDetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -4,16 +4,20 @@ import {
   CardImg,
   CardImgOverlay,
   CardTitle,
+  Breadcrumb,
+  BreadcrumbItem
 } from 'reactstrap';
-
+import { Link } from 'react-router-dom';
 
 function RenderMenuItem({dish, onClick}) {
   return (
     <Card>
+      <Link to={`/menu/${dish.id}`}>
       <CardImg width="100%" src={dish.image} alt={dish.name} />
       <CardImgOverlay>
         <CardTitle>{dish.name}</CardTitle>
       </CardImgOverlay>
+      </Link>
     </Card>
   );
 }
@@ -24,12 +28,22 @@ const Menu = (props) => {
     const menu = props.dishes.map((dish) => {
       return (
         <div key={dish.id} className="col-12 col-md-5 m-1">
-          <RenderMenuItem dish={dish} onClick={props.onClick}/>
+          <RenderMenuItem dish={dish}/>
         </div>
       );
     });
     return (
       <div className="container">
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+            <BreadcrumbItem active>Menu</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>Menu</h3>
+            <hr/>
+          </div>
+        </div>
         <div className="row">{menu}</div>
       </div>
     );
