66c2e6dd35f662669b21f3990c0c4b726fa9f10a
Sarvar <r0ssmike727@yandex.com>
2020-10-03 17:08:09 +0500

    Finished the course

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -69,10 +69,6 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   componentDidMount() {
     this.props.fetchDishes();
     this.props.fetchComments();
